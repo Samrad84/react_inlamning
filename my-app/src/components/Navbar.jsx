713e1e47248cf7bcd8c62a074d90a6833dd705d5
sammiths <112378887+sammiths@users.noreply.github.com>
2022-09-08 08:52:43 +0200

    fixed some CSS and design

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 
+
 const Navbar  = () =>  {
 
 return <nav>
     <div className="logo">
-        <i className="fas fa-scored "></i>
-        <h4>Scored</h4>
+       
+        <h4>Sc<i className="fa-solid fa-futbol"></i>reB<i className="fa-solid fa-futbol"></i>ard</h4>
 
     </div>
-    ,<ul className="navlinks">
+    <ul className="navlinks">
         <li className="link"> <a href="#">Home</a></li>
         <li className="link"> <a href="#">Players</a></li>
         <li className="link"> <a href="#">Matches</a></li>
         <li className="link"> <a href="#">Contact</a></li>
     </ul>
+    <div className="hamburger-toggle">
+       <i className="fas fa-bars fa-lg"></i> 
+    </div>
 </nav>
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,21 +1,18 @@
-import react from "react";
-import  ReactDOM  from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
 
 
-const App = ()  => {
+const App = () => {
     return (
         <div>
-            <Navbar/>
-            <div className="main">
-
-
-            </div>
+            <Navbar />
+            <div className="main"></div>
         </div>
-    )
-}
+    );
+};
 
 
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
