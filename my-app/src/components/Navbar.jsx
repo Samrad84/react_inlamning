62214d143ded50a802cba37759c04a2b384e8c5c
sammiths <112378887+sammiths@users.noreply.github.com>
2022-09-12 12:21:57 +0200

    Fixed the HamB nav(Responsive)

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+const Navbar = () => {
+    const [navLinkOpen, navLinkToggle] = useState(false);
 
 
-const Navbar  = () =>  {
+    const handleNavLinksToggle = () => {
+        navLinkToggle(!navLinkOpen)
+};
+const renderClasses = ()  => {
 
-return <nav>
-    <div className="logo">
-       
-        <h4>Sc<i className="fa-solid fa-futbol"></i>reB<i className="fa-solid fa-futbol"></i>ard</h4>
+    let classes = "navlinks";
+    if(navLinkOpen){
+        classes += " activ"
+    }
+    return classes;
 
-    </div>
-    <ul className="navlinks">
-        <li className="link"> <a href="#">Home</a></li>
-        <li className="link"> <a href="#">Players</a></li>
-        <li className="link"> <a href="#">Matches</a></li>
-        <li className="link"> <a href="#">Contact</a></li>
-    </ul>
-    <div className="hamburger-toggle">
-       <i className="fas fa-bars fa-lg"></i> 
-    </div>
-</nav>
-}
+
+};
+
+
+return (
+        <nav>
+            <div className="logo">
+                <h4>Sc<i className="fa-solid fa-futbol"></i>reB<i className="fa-solid fa-futbol"></i>ard</h4>
+            </div>
+            <ul className={renderClasses()}>
+                <li className="link"> <a href="#">Home</a></li>
+                <li className="link"> <a href="#">Players</a></li>
+                <li className="link"> <a href="#">Matches</a></li>
+                <li className="link"> <a href="#">Contact</a></li>
+            </ul>
+            <div  onClick={handleNavLinksToggle} className="hamburger-toggle">
+                <i className="fas fa-bars fa-lg"></i>
+            </div>
+        </nav>
+    );
+};
 
 export default Navbar;
